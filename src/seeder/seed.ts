d4b96b20b502ae4d5a21294b18e5372ec81f09f0
Daniel <Daniel.halaas@gmail.com>
2025-11-19 18:52:46 +0100

    Create a seeder to run in prod database

---
+++ src/seeder/seed.ts
@@ -0,0 +1,62 @@
+// seeder for 'scape' prod database
+import mongoose from 'mongoose';
+import config from '../config/config.ts';
+import Item from '../models/Item.ts';
+
+async function seed() {
+    try {
+        await mongoose.connect(config.mongoDBUri);
+
+        console.log('Clearing old items...');
+        await Item.deleteMany({});
+
+        console.log("Inserting items...");
+        await Item.insertMany([
+            {
+                type: "frame",
+                label: "Oak",
+                value: "oak",
+                price: 99,
+            },
+            {
+                type: "frame",
+                label: "Walnut",
+                value: "walnut",
+                price: 99,
+            },
+            {
+                type: "frame",
+                label: "White",
+                value: "white",
+                price: 99,
+            },
+            {
+                type: "frame",
+                label: "Black",
+                value: "black",
+                price: 99,
+            },
+            {
+                type: "passepartout",
+                label: "White",
+                value: "white",
+                price: 49,
+            },
+            {
+                type: "passepartout",
+                label: "Black",
+                value: "black",
+                price: 49,
+            }
+        ]);
+        
+        console.log('Seeding complete');
+    } catch (err) {
+        console.error('Failed to seed:', err);
+        process.exit(1);
+    } finally {
+        await mongoose.disconnect();
+    }
+}
+
+seed();
