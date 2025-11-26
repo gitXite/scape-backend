9ccd82b21d11159aef91a06c0f7269f315128283
gitXite <daniel.halaas@gmail.com>
2025-11-26 13:19:05 +0100

    typo in preflight function call

--- api/contact/index.ts
+++ api/contact/index.ts
@@ -4,7 +4,7 @@ import enableCors from '../../dist/utils/enableCors.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     enableCors(res);
-    if (req.method === 'OPTIONS') return res.status(200).end;
+    if (req.method === 'OPTIONS') return res.status(200).end();
     
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'HTTP method not allowed' });
