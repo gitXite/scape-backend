7a9346c29f1403846273cc4573a01e6688e4c43e
gitXite <daniel.halaas@gmail.com>
2025-11-29 21:51:32 +0100

    add data to order schema

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -2,7 +2,32 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const orderSchema = new Schema({
-    orderID: String,
+    orderID: {
+        type: String,
+        required: true,
+    },
+    coordinates: {
+        north: { type: Number, required: true },
+        south: { type: Number, required: true },
+        east: { type: Number, required: true },
+        west: { type: Number, required: true },
+    },
+    scale: {
+        type: Number,
+        required: true,
+    },
+    verticalScale: {
+        type: Number,
+        required: true,
+    },
+    frame: {
+        type: String,
+        required: true,
+    },
+    passepartout: {
+        type: String,
+        required: true,
+    },
     // the rest from Vipps
 }, {
     timestamps: true,
