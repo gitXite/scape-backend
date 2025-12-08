dc4e4268ac06f52c20a5f748065fb6fac1cdb80a
gitXite <daniel.halaas@gmail.com>
2025-12-08 19:31:11 +0100

    generalize util function

--- src/utils/generateID.ts
+++ src/utils/generateID.ts
@@ -1,8 +1,8 @@
-export function generateCaseID(): string {
+export function generateID(): string {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let id = '';
     for (let i = 0; i < 6; i++) {
         id += chars.charAt(Math.floor(Math.random() * chars.length));
     }
-    return `CASE #${id}`;
+    return id;
 }
