9411b2ad2083f89321eb6bca8415848db0bad1c5
gitXite <daniel.halaas@gmail.com>
2025-12-12 13:12:30 +0100

    add order info to order schema

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -1,52 +1,59 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-const orderSchema = new Schema({
-    orderID: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    status: {
-        type: String,
-        default: "PENDING",
-    },
-    amount: {
-        type: Number,
-        required: true,
-    },
-    paymentReference: { type: String },
+const orderSchema = new Schema(
+    {
+        orderID: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        status: {
+            type: String,
+            default: 'PENDING',
+        },
+        amount: {
+            type: Number,
+            required: true,
+        },
+        paymentReference: { type: String },
 
-    customerName: { type: String },
-    customerEmail: { type: String },
-    customerPhone: { type: String },
-    shippingAddress: { type: String },
+        customerFirstName: { type: String },
+        customerLastName: { type: String },
+        customerEmail: { type: String },
+        customerPhone: { type: String },
+        shippingAddress: { type: String },
+        postalCode: { type: String },
+        city: { type: String },
+        shippingMethod: { type: String },
 
-    coordinates: {
-        north: { type: Number, required: true },
-        south: { type: Number, required: true },
-        east: { type: Number, required: true },
-        west: { type: Number, required: true },
+        coordinates: {
+            north: { type: Number, required: true },
+            south: { type: Number, required: true },
+            east: { type: Number, required: true },
+            west: { type: Number, required: true },
+        },
+        scale: {
+            type: Number,
+            required: true,
+        },
+        verticalScale: {
+            type: Number,
+            required: true,
+        },
+        frame: {
+            type: String,
+            required: true,
+        },
+        passepartout: {
+            type: String,
+            required: true,
+        },
     },
-    scale: {
-        type: Number,
-        required: true,
-    },
-    verticalScale: {
-        type: Number,
-        required: true,
-    },
-    frame: {
-        type: String,
-        required: true,
-    },
-    passepartout: {
-        type: String,
-        required: true,
-    },
-}, {
-    timestamps: true,
-});
+    {
+        timestamps: true,
+    }
+);
 
 const Order = model('Order', orderSchema);
 export default Order;
