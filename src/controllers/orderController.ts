311b6c8bd3dccb226df54f1ff82bcf4cc52a2c83
gitXite <daniel.halaas@gmail.com>
2025-12-12 13:12:05 +0100

    add order information to email template

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -2,38 +2,84 @@ import type { NextFunction, Request, Response } from 'express';
 import { generateSTL } from '../services/terrainService';
 import { sendMail } from '../services/emailService';
 
-export const sendSTL = async (req: Request, res: Response, next: NextFunction) => {
-    // maybe get the whole order request here with address and so forth
-    // example parameters, probably needs to change, with typing
-    const { lat, lng, verticalScale, scale, orderID, name, email } = req.body;
-    if (!lat || !lng || !verticalScale || !scale /*|| !orderID || !name || !email*/) {
+interface Order {
+    orderId: string;
+    status?: string;
+    amount?: number;
+
+    customerFirstName: string;
+    customerLastName: string;
+    customerEmail: string;
+    customerPhone: string;
+    shippingAddress: string;
+    postalCode: string;
+    city: string;
+    shippingMethod: string;
+
+    coordinates: {
+        north: number;
+        south: number;
+        east: number;
+        west: number;
+    };
+    verticalScale: number;
+    scale: number;
+    frame: string;
+    passepartout: string;
+}
+
+export const sendSTL = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const order = req.body as Order;
+    if (
+        !order.orderId ||
+        !order.coordinates ||
+        !order.verticalScale ||
+        !order.scale ||
+        !order.frame ||
+        !order.passepartout ||
+        !order.customerFirstName ||
+        !order.customerLastName ||
+        !order.customerEmail ||
+        !order.customerPhone ||
+        !order.shippingAddress ||
+        !order.postalCode
+    ) {
         return res.status(400).json({ message: 'Missing required fields' });
     }
 
     try {
-        const stlBuffer = await generateSTL({ lat, lng, verticalScale, scale });
+        const stlBuffer = await generateSTL({ lat: order.coordinates.north, lng: order.coordinates.west, verticalScale: order.verticalScale, scale: order.scale });
 
         await sendMail({
             to: 'scapebymd@gmail.com',
-            subject: `ORDER PLACED # ${orderID}`,
-            text: `Name: ${name} Email: ${email}`,
+            subject: `ORDER PLACED # ${order.orderId}`,
+            text: `Name: ${order.customerFirstName} ${order.customerLastName} Email: ${order.customerEmail}`,
             template: 'stlFileEmail',
             templateVars: {
-                'ORDER_ID': orderID,
-                'CUSTOMER_NAME': name,
-                'CUSTOMER_EMAIL': email,
-                'ORDER_DATE': new Date().toLocaleString(),
-                'CURRENT_YEAR': `${new Date().getFullYear()}`,
+                ORDER_ID: order.orderId,
+                CUSTOMER_NAME: `${order.customerFirstName} ${order.customerLastName}`,
+                CUSTOMER_PHONE: order.customerPhone,
+                CUSTOMER_EMAIL: order.customerEmail,
+                SHIPPING_ADDRESS: order.shippingAddress,
+                POSTAL_CODE: `${order.postalCode}, ${order.city}`,
+                FRAME: order.frame,
+                PASSEPARTOUT: order.passepartout,
+                ORDER_DATE: new Date().toLocaleString(),
+                CURRENT_YEAR: `${new Date().getFullYear()}`,
             },
             attachments: [
                 {
-                    filename: `${orderID}.stl`,
+                    filename: `${order.orderId}.stl`,
                     content: stlBuffer,
                     contentType: 'application/sla',
-                }
-            ]
+                },
+            ],
         });
-        res.status(200).json({ message: 'STL sent successfully' });
+        res.status(200).end;
     } catch (err: any) {
         if (err.status === 429) {
             return res.status(429).json({ message: err.message });
