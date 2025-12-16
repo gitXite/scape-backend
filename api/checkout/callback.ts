6299958549072ec461a84c3ce9fee1957545e372
Daniel <Daniel.halaas@gmail.com>
2025-12-16 09:07:18 +0100

    Fix OPTIONS response end method call

--- api/checkout/callback.ts
+++ api/checkout/callback.ts
@@ -6,7 +6,7 @@ import connectDB from '../../dist/config/db.js';
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     await connectDB();
     enableCors(req, res);
-    if (req.method === 'OPTIONS') return res.status(200).end;
+    if (req.method === 'OPTIONS') return res.status(200).end();
 
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'HTTP method not allowed' });
