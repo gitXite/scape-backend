82e23e53e88be8d603dd1ff00d283a4d3191d884
Daniel <Daniel.halaas@gmail.com>
2025-12-16 09:07:09 +0100

    Fix OPTIONS response end method call

--- api/checkout/index.ts
+++ api/checkout/index.ts
@@ -6,7 +6,7 @@ import connectDB from '../../dist/config/db.js';
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     await connectDB();
     enableCors(res);
-    if (req.method === 'OPTIONS') return res.status(200).end;
+    if (req.method === 'OPTIONS') return res.status(200).end();
 
     switch (req.method) {
         case 'POST':
